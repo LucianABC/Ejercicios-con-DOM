158ab6c2b4d86e2c9ac204b8ba01f409f38e049c
LucianABC <louabecasis@gmail.com>
2019-12-03 11:12:13 -0300

    ej dom

---
+++ script.js
@@ -0,0 +1,27 @@
+toggleChecked = () => {
+    if (document.querySelector("#Check").checked) {
+        document.querySelector("#Check").checked = false;
+    } else {
+        document.querySelector("#Check").checked = true;
+    }
+};
+
+cambiarNombre= ()=> {
+    let elemento = document.querySelector("#miInput");
+    elemento.name="miExInput";
+};
+
+
+crearLista = () => {
+    let elemento =  document.querySelector("#miDiv");
+    elemento.innerHTML = "<ul> <li></li> <li></li> <li></li> <li></li> <li></li> </ul>";
+};
+
+llenarLista = () => {
+    let lista = document.getElementsByTagName("li");
+    let valor = 0;
+    for (let li of lista){
+        valor++;
+        li.innerHTML =  valor;
+    };
+};
