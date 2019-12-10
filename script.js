2ca526097adf52a16835fb6250b3b9dbb9df5eae
LucianABC <louabecasis@gmail.com>
2019-12-10 12:49:33 -0300

    Ejercicios eventos 1

--- script.js
+++ script.js
@@ -106,14 +106,14 @@ const addActive = (indice) => {
     const increaseTitle = (titulo) => { 
         let title = document.querySelector(titulo);
         let fontSize = getComputedStyle(title).getPropertyValue("font-size");
-        fontSize= parseFloat(fontSize);
+        fontSize= parseInt(fontSize);
         title.style.fontSize = fontSize + 10 + "px";
     }
 
     const decreaseTitle = (titulo) => { 
         let title = document.querySelector(titulo);
         let fontSize = getComputedStyle(title).getPropertyValue("font-size");
-        fontSize= parseFloat(fontSize);
+        fontSize= parseInt(fontSize);
         title.style.fontSize = fontSize - 10 + "px";
     }
 
@@ -142,3 +142,69 @@ const hideImg =() => {
         imagenes[indice+1].classList.add("display");
     
 } 
+//////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////
+// 001 
+/**
+ * Crear un TODO list, que tenga
+ * 1. un input para agregar nuevos TODO
+ * 2. Este input debe tener un button para poder
+ *    agregar el TODO desde el input
+ * 3. un button por cada TODO de la lista
+ *  para borrar el mismo.
+ * 4. Un checkbox, antes del texto de cada TODO,
+ *  que me permita tachar el elemento de la lista
+ *  como resuelto. 
+ */
+//1.
+    let toDoList = document.querySelector("#toDo-list");
+    const addButton = document.querySelector("#addButton");
+    
+    const addTask = event => {
+        let li = document.createElement("li");
+        li.className="toDo-el";
+        
+        let checkbox = document.createElement("input");
+        checkbox.type="checkbox";
+        checkbox.id="done";
+        li.appendChild(checkbox)
+        
+        let p = document.createElement("p");
+        let texto = document.querySelector("#newToDo");
+        p.innerHTML=texto.value;
+        li.appendChild(p);
+
+        let deleteButton = document.createElement("button");
+        deleteButton.type="submit";
+        deleteButton.id="deleteItem";
+        deleteButton.innerHTML="Eliminar";
+        li.appendChild(deleteButton);
+
+        toDoList.appendChild(li);
+    }
+
+    addButton.onclick=addTask; //si hago click funciona, ver como hacer para que cuando pongo enter tambien
+ //4.
+ const checkbox = document.querySelector("#done");
+ const taskDone= event => {
+     const toDoItem= checkbox.parentElement;
+     "toDoItem > li"
+
+
+ }
+
+
+ // 002
+/**
+ * Tomar el input del ejercicio anterior y hacer que 
+ * cambie sus bordes de color y de ancho cuando 
+ * hacemos foco(focus, hacemos click en el recuadro) en el, 
+ * y que esos mismos bordes y color vuelvan al 
+ * estado anterior cuando sacamos el foco (blur).
+ */
+
+ // 003
+ /**
+  * 
+  */
